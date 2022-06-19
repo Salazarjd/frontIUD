fd76528d909f289083a54004d5660ba00f6e437e
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-06-19 14:04:50 -0500

    inventarios  ok

--- src/components/inventario/Inventario.js
+++ src/components/inventario/Inventario.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import Tabla from './Tabla'
-import { obtenerTodos } from '../../services/InventarioService';
-import Modal from './Modal';
+import React, { useEffect, useState } from "react";
+import Tabla from "./Tabla";
+import { obtenerTodos } from "../../services/InventarioService";
+import Modal from "./Modal";
 
 export default function Inventario() {
-
   const [inventarios, setInventarios] = useState([]);
   const [inventario, setInventario] = useState({
     serial: "",
@@ -17,49 +16,44 @@ export default function Inventario() {
     usuario: "",
     marca: "",
     estado: "",
-    tipoEquipo: ""
+    tipoEquipo: "",
   });
-  
 
-  useEffect( () => {
+  useEffect(() => {
     const getInventarios = () => {
-        obtenerTodos()
-        .then(r => {
-            console.log(r);
-            setInventarios(r.data)
-        }).catch(e => {
-            console.log(e)
+      obtenerTodos()
+        .then((r) => {
+          console.log(r.data);
+          setInventarios(r.data.inventarios);
         })
-    }
+        .catch((e) => {
+          console.log(e);
+        });
+    };
     getInventarios();
   }, []);
 
-  const changeInventario = e => {
+  const changeInventario = (e) => {
     e.preventDefault();
     setInventario({
       ...inventario,
-      [e.target.name]: e.target.value 
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <div className='container'>
-      <button 
-        type="button" 
+    <div className="container">
+      <button
+        type="button"
         className="btn btn-outline-primary"
-        data-bs-toggle="modal" 
+        data-bs-toggle="modal"
         data-bs-target="#modalInventarios"
       >
         <i className="fa-solid fa-plus"></i>
         Agregar
       </button>
-      <Tabla 
-        inventarios={inventarios}
-      />
-      <Modal 
-        inventario={inventario}
-        changeInventario={changeInventario}
-      />
+      <Tabla inventarios={inventarios} />
+      {/* <Modal inventario={inventario} changeInventario={changeInventario} /> */}
     </div>
-  )
+  );
 }
--- src/services/InventarioService.js
+++ src/services/InventarioService.js
@@ -1,7 +1,13 @@
-import { axiosConfig } from "../config/axiosConfig"
+import { axiosConfig } from "../config/axiosConfig";
 
 export const obtenerTodos = () => {
-  return axiosConfig.get(
-      '/inventarios'
-  );
-}
+  return axiosConfig.get("/inventarios");
+};
+
+export const guardar = (estado) => {
+  return axiosConfig.post("/inventarios", estado);
+};
+
+export const editarPorId = (id, estado) => {
+  return axiosConfig.put("/inventarios/" + id, estado);
+};
